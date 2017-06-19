8ca6b35cdfe21ce80e8016f8cfa7f68e69bc04a9
André <andrealencar@outlook.com.br>
2017-06-19 15:16:02 +0000

    Remoção da obrigatoriedade de status e ajuste das validações (em caso de campos nulos não comparar)

--- app/models/laboratory.rb
+++ app/models/laboratory.rb
@@ -1,25 +1,29 @@
 class Laboratory < ApplicationRecord
-  validates :maximum_capacity, :amount_resources, :status, :name, :initials, presence: true
+  validates :maximum_capacity, :amount_resources, :name, :initials, presence: true
   validates :maximum_capacity, :numericality => { :greater_than_or_equal_to => 0 }
-    validates :amount_resources, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :amount_resources, :numericality => { :greater_than_or_equal_to => 0 }
   validate :exist_other_with_this_initials
   validate :exist_other_with_this_name
 
   def exist_other_with_this_initials
     @laboratories = Laboratory.all
+    unless initials.nil?
     @laboratories.each { |l|
       unless l.id == id
         errors.add(:initials, 'Outro laboratório já possui sigla informada') if l.initials.casecmp(initials) == 0
       end
     }
+    end
   end
 
   def exist_other_with_this_name
     @laboratories = Laboratory.all
+    unless name.nil?
     @laboratories.each { |l|
       unless l.id == id
         errors.add(:initials, 'Outro laboratório já possui nome informada') if l.name.casecmp(name) == 0
       end
     }
+    end
   end
 end
