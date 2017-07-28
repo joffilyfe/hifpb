61ede40f0c925d69b6dcb6606ecccb46b3e53112
Andre Alencar <andrealencar@outlook.com.br>
2017-07-28 16:33:20 -0300

    Ajuste nas migrações, e filtragem nos formularios de cadastro de turmas

--- db/migrate/20170723123503_create_permissions.rb
+++ db/migrate/20170723123503_create_permissions.rb
@@ -1,5 +1,7 @@
 class CreatePermissions < ActiveRecord::Migration[5.1]
   def change
+    drop_table :authorizations
+
     create_table :permissions do |t|
       t.string :title
       t.string :module
@@ -11,58 +13,52 @@ class CreatePermissions < ActiveRecord::Migration[5.1]
     create_join_table :permissions, :users do |t|
       t.index [:permission_id, :user_id]
     end
-
-    # Authorizations
-    Permission.create({title: "Can see all Authorizations", module: "authorization", action: "index"})
-    Permission.create({title: "Can create Authorizations", module: "authorization", action: "create"})
-    Permission.create({title: "Can delete Authorizations", module: "authorization", action: "destroy"})
-    Permission.create({title: "Can see Authorizations", module: "authorization", action: "new"})
     
     # Campus
-    Permission.create({title: "Can see all Campus", module: "campus", action: "index"})
-    Permission.create({title: "Can import Campus", module: "campus", action: "import"})
+    Permission.create({title: "Pode ver home de Campus", module: "campus", action: "index"})
+    Permission.create({title: "Pode importar Campus", module: "campus", action: "import"})
     
     # Campus Schedule
-    Permission.create({title: "Can see all Campus Schedules", module: "campus_schedule", action: "index"})
-    Permission.create({title: "Can create Campus Schedules", module: "campus_schedule", action: "create"})
-    Permission.create({title: "Can delete Campus Schedules", module: "campus_schedule", action: "destroy"})
+    Permission.create({title: "Pode ver home de horarios", module: "campus_schedule", action: "index"})
+    Permission.create({title: "Pode criar horarios", module: "campus_schedule", action: "create"})
+    Permission.create({title: "Pode remover horarios", module: "campus_schedule", action: "destroy"})
     
     # Classroom
-    Permission.create({title: "Can see all Classrooms", module: "classroom", action: "index"})
-    Permission.create({title: "Can see a Classroom", module: "classroom", action: "show"})
-    Permission.create({title: "Can create Classrooms", module: "classroom", action: "create"})
-    Permission.create({title: "Can edit Classroom", module: "classroom", action: "edit"})
-    Permission.create({title: "Can delete Classroom ", module: "classroom", action: "destroy"})
+    Permission.create({title: "Pode ver home de turmas", module: "classroom", action: "index"})
+    Permission.create({title: "Pode ver uma turma", module: "classroom", action: "show"})
+    Permission.create({title: "Pode criar turmas", module: "classroom", action: "create"})
+    Permission.create({title: "Pode editar turmas", module: "classroom", action: "edit"})
+    Permission.create({title: "Pode remover turmas", module: "classroom", action: "destroy"})
     
     # Course
-    Permission.create({title: "Can see all Courses", module: "course", action: "index"})
-    Permission.create({title: "Can import Courses", module: "course", action: "import"})
+    Permission.create({title: "Pode ver home de cursos", module: "course", action: "index"})
+    Permission.create({title: "Pode importar cursos", module: "course", action: "import"})
     
     # CourseSubject
-    Permission.create({title: "Can see all Course Subjects", module: "course_subject", action: "index"})
-    Permission.create({title: "Can import Course Subjects", module: "course_subject", action: "import"})
+    Permission.create({title: "Pode ver home de disciplinas", module: "course_subject", action: "index"})
+    Permission.create({title: "Pode importar disciplinas", module: "course_subject", action: "import"})
     
     # Laboratory
-    Permission.create({title: "Can see all Courses", module: "laboratory", action: "index"})
-    Permission.create({title: "Can see all Courses", module: "laboratory", action: "show"})
-    Permission.create({title: "Can see all Courses", module: "laboratory", action: "create"})
-    Permission.create({title: "Can see all Courses", module: "laboratory", action: "edit"})
-    Permission.create({title: "Can see all Courses", module: "laboratory", action: "destroy"})
+    Permission.create({title: "Pode ver home de laboratorios", module: "laboratory", action: "index"})
+    Permission.create({title: "Pode ver laboratorios", module: "laboratory", action: "show"})
+    Permission.create({title: "Pode criar laboratorios", module: "laboratory", action: "create"})
+    Permission.create({title: "Pode editar laboratorios", module: "laboratory", action: "edit"})
+    Permission.create({title: "Pode remover laboratorios", module: "laboratory", action: "destroy"})
     
     # Schoolroom
-    Permission.create({title: "Can see all Courses", module: "schoolroom", action: "index"})
-    Permission.create({title: "Can see all Courses", module: "schoolroom", action: "show"})
-    Permission.create({title: "Can see all Courses", module: "schoolroom", action: "create"})
-    Permission.create({title: "Can see all Courses", module: "schoolroom", action: "edit"})
-    Permission.create({title: "Can see all Courses", module: "schoolroom", action: "destroy"})
+    Permission.create({title: "Pode ver home de salas", module: "schoolroom", action: "index"})
+    Permission.create({title: "Pode ver sala", module: "schoolroom", action: "show"})
+    Permission.create({title: "Pode criar salas", module: "schoolroom", action: "create"})
+    Permission.create({title: "Pode editar salas", module: "schoolroom", action: "edit"})
+    Permission.create({title: "Pode remover salas", module: "schoolroom", action: "destroy"})
     
     # Semester
-    Permission.create({title: "Can see all Semesters", module: "semester", action: "index"})
-    Permission.create({title: "Can create Semesters", module: "semester", action: "create"})
+    Permission.create({title: "Pode ver home de os semestres", module: "semester", action: "index"})
+    Permission.create({title: "Pode criar semestres", module: "semester", action: "create"})
     
     # Teacher
-    Permission.create({title: "Can see all imported teachers", module: "teacher", action: "index"})
-    Permission.create({title: "Can import Teacher", module: "teacher", action: "import"})
-  end
+    Permission.create({title: "Pode ver home de professores", module: "teacher", action: "index"})
+    Permission.create({title: "Pode importar professores", module: "teacher", action: "import"})
   
+  end 
 end
