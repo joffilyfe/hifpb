5473f38514e9e763d87dc4a526961d5b2f3b5729
Marianna Veríssimo <marianna1204@gmail.com>
2017-07-28 13:02:42 -0300

    Ajustes - Turma :tipping_hand_woman:
    Remoção de campos do form da criação da turma. Associação da turma com o Semestre.

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -1,5 +1,9 @@
 class Semester < ApplicationRecord
   validates :year, :semester, presence: true, on: :create
-	validates :year, uniqueness: { scope: :semester,
-    message: "should not have same year and semester togeter" }
+  validates :year, uniqueness: { scope: :semester, message: "should not have same year and semester togeter" }
+  has_many :classrooms
+
+  def to_s
+    "#{year}.#{semester}"
+  end
 end
